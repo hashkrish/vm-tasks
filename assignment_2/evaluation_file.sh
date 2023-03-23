716d0d6f099a1a9a8f85af1a58ed4e9862b7bb88
santhana krishnan <29514440+hashkrish@users.noreply.github.com>
2023-03-23 16:09:31 +0530

    assignment 2 upload a file to server

---
+++ assignment_2/evaluation_file.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if ! [ -f ~/se2001/assignment_2/script.sh ]; then
+	echo "File $HOME/se2001/assignment_2/script.sh not found" >&2
+	exit 1
+fi
+
+if ! [ -x ~/se2001/assignment_2/script.sh ]; then
+	echo "File $HOME/se2001/assignment_2/script.sh is not executable" >&2
+	exit 1
+fi
+
+~/se2001/assignment_2/script.sh
+
+if test -f "/tmp/local_uploads/$(whoami)"; then
+	exit 0
+else
+	echo "File not uploaded!" >&2
+	echo "Check the URL that you have sent the file!" >&2
+	exit 1
+fi
