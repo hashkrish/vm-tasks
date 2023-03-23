010877dd972a883fbd7ba4cd443fa93ac03730a4
prass <vidyaaltar@gmail.com>
2023-03-23 17:09:06 +0600

    modified evaluation code

--- assignment_3/evaluation_file.sh
+++ assignment_3/evaluation_file.sh
@@ -10,29 +10,18 @@ if ! [ -x ~/se2001/assignment_3/script.sh ]; then
 	exit 1
 fi
 
-fib() {
-        echo "
-        define fibonacci(x) {
-                fib0 = 0
-                fib1 = 1
-                for (i = 0; i < x - 1; i++) {
-                        fib = fib0 + fib1
-                        fib0 = fib1
-                fib1 = fib
-                }
-                return fib0
-        }
-        fibonacci($1)
-        " | bc
-}
+declare -A hash
+hash[49]=4807526976
+hash[83]=61305790721611591
+hash[121]=5358359254990966640871840
+hash[21]=6765
+hash[50]=7778742049
 
-
-random_number=( `shuf -i 1-100 -n 3` )
-for i in ${random_number[@]}; do
+for i in ${!hash[@]}; do
+        
         sfibnum=$(bc -q fibonacci.bc <<< "fibonacci($i)")
-        efibnum=$(fib $i)
 
-        if [[ "$sfibnum" -ne "$efibnum" ]];
+        if [[ "$sfibnum" -ne "${hash[$i]}" ]];
 	then
                 echo "Fibonacci evaluation failed" >&2;
                 exit 1
