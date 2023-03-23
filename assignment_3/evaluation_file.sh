0bceda748a6d9f494279c3bc821111f4bb9bdfe9
prass <vidyaaltar@gmail.com>
2023-03-23 16:21:54 +0600

    modified evaluation criteria

--- assignment_3/evaluation_file.sh
+++ assignment_3/evaluation_file.sh
@@ -25,18 +25,17 @@ fib() {
         fibonacci($1)
         " | bc
 }
-random_number=`shuf -i 1-200 -n 1`
-
-sfibnum=`./script.sh $random_number`
-
-efibnum=`fib $random_number`
-
-if [[ "$sfibnum" -eq "$efibnum" ]];
-then	
-	echo "Fibonacci number evaluation successful";
-else
-	echo "Fibonacci number evaluation failed" >&2;
-	exit 1
-fi
 
 
+random_number=( `shuf -i 1-100 -n 3` )
+for i in ${random_number[@]}; do
+        sfibnum=$(bc -q fibonacci.bc <<< "fibonacci($i)")
+        efibnum=$(fib $i)
+
+        if [[ "$sfibnum" -ne "$efibnum" ]];
+	then
+                echo "Fibonacci evaluation failed" >&2;
+                exit 1
+        fi
+done
+echo "Fibonacci evaluation successful"
