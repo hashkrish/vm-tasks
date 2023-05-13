07d800ab3a4829433ceca22162484cba808d6095
prass <92257326+prassr@users.noreply.github.com>
2023-05-13 14:01:37 +0600

    Problem 6.5 evaluation file modified

--- practice_6.5/evaluation_file.sh
+++ practice_6.5/evaluation_file.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-test_dir="/opt/se2001/practice_6.5/test_case_1"
+test_dir="/opt/se2001/practice_6.5"
 
 i=0
 
-cp $test_dir/input sedintro.html
+for dir in "${test_dir}/test_case_*";
+do
 
-sed -n "${OPTS[@]}" -f script.sed sedintro.html > out.txt
-        
-diff out.txt $dir/output &> /dev/null
+        cp $dir/input sedintro.html
 
-if [ $? -eq 1 ];
-then
-    echo "Test case $(( i+1 )) failed"
-    exit 1
-fi
+        sed -n "${OPTS[@]}" -f script.sed sedintro.html > out.txt
+
+        diff out.txt $dir/output &> /dev/null
+
+        if [ $? -eq 1 ];
+        then
+                echo "Test case $(( i+1 )) failed"
+                exit 1
+        fi
+	i=$(( i+1 ))
+done
+
