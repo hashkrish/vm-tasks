91eccd1494e8a876aee01e8e50bc1951e8293683
prass <92257326+prassr@users.noreply.github.com>
2023-05-31 12:48:30 +0600

    files renamed output > getop

--- practice_3.5/evaluation_file.sh
+++ practice_3.5/evaluation_file.sh
@@ -5,8 +5,7 @@ test_dir="/opt/se2001/$(basename $(pwd))"
 for dir in $(ls $test_dir/ | grep "test_case" | sort); do
 	read cmd <$test_dir/$dir/input
 	cmd="$cmd" bash script.sh
-	cmd="$cmd" bash $test_dir/$dir/output
+	cmd="$cmd" bash $test_dir/$dir/getop
 	diff ans.txt man_$cmd.txt &>/dev/null || exit 1
 	diff ans.err man_$cmd.err &>/dev/null || exit 1
-	((i++))
 done
